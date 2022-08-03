5771fd10b766140f9ac7e25ee2e2a9f2b424c447
Anthony T <anthonyt3@sbcglobal.net>
2022-08-03 10:53:20 -0700

    User & Chan Seeders

---
+++ app/seeds/channels.py
@@ -0,0 +1,24 @@
+from app.models import db, Channel
+# emtpy seed file template: name= Song(
+#   name='', album='', albumImgUrl='', genre='', artist='', source='', user_Id=)
+def seed_channels():
+    demo_team = Channel(
+        title='Demo Team', description='The first team on Simul.', user_id=1)
+    money_team = Channel(
+        title='Money Team', description='Get Money Or Get Out!', user_id=2)
+    tp_management = Channel(
+        title='Trailer Park Management', description='The Few, The Proud.', user_id=3)
+    bobby_boys = Channel(
+        title='Bobby Boys', description='', user_id=4)
+
+    db.session.add(demo_team)
+    db.session.add(money_team)
+    db.session.add(tp_management)
+    db.session.add(bobby_boys)
+
+    db.session.commit()
+
+
+def undo_channels():
+    db.session.execute('TRUNCATE songs RESTART IDENTITY CASCADE;')
+    db.session.commit()
