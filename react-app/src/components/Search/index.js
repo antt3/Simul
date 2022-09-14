ae73f6a16e3ca6361a3019242336da0fd4bb16a0
Anthony T <anthonyt3@sbcglobal.net>
2022-09-13 19:58:20 -0700

    Created Search Folder & Files

---
+++ react-app/src/components/Search/index.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+import './Search.css';
+
+
+const Search = () => {
+    const [search, setSearch] = useState('');
+
+    return (
+        <></>
+    );
+};
+
+export default Search;
