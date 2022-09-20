0afd8d4327a5b32cd9c6a26c4a4e62b51349cbf6
Anthony T <anthonyt3@sbcglobal.net>
2022-09-20 01:42:18 -0700

    Maps For Search Results

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -4,8 +4,10 @@ import { Redirect, useHistory, useParams } from 'react-router-dom';
 
 import * as dmReducer from '../../store/directMessages';
 import * as channelsReducer from '../../store/channels';
+import defaultProfileImage from '../../default_profile_image.jpg';
 
-
+import '../DirectMessages/DirectMessages.css';
+import '../ChannelChat/ChannelChat.css';
 import './Search.css';
 
 
@@ -17,6 +19,7 @@ const Search = () => {
     const channelsArray = Object.values(channels);
     const { searchTerm } = useParams();
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const [users, setUsers] = useState([]);
     const [search, setSearch] = useState(searchTerm);
@@ -76,6 +79,25 @@ const Search = () => {
 		fetchData();
 	}
 
+    const onClickUser = (e, user) => {
+        e.stopPropagation();
+        history.push(`/users/${user.id}`);
+    };
+
+    const onClickChannel = (e, channel) => {
+        e.stopPropagation();
+        history.push(`/channels/${channel.id}`);
+    };
+
+    const onClickDM = (e, user) => {
+        e.stopPropagation();
+        history.push(`/direct-messages/${user.id}`);
+    };
+
+    const createdAt = (timestamp) => {
+        return timestamp.split('.')[0];
+    }
+
     if (!currentUser) return <Redirect to="/splash" />;
 
 
@@ -91,28 +113,103 @@ const Search = () => {
 				/>
                 <button>Search</button>
 			</form>
-            {searchedUsers.length > 0 && (
-				<div className='users_div'>
-					<h2>Users: </h2>
-					<div className="users_divs">{searchedUsers}</div>
-				</div>
-			)}
+
             {searchedChannels.length > 0 && (
 				<div className='users_div'>
 					<h2>Channels: </h2>
-					<div className="users_divs">{searchedChannels}</div>
+					<div className="users_divs">
+                        {searchedChannels.map((channel) => (
+                            <div className='user_div' onClick={(e, channel) => onClickChannel(e, channel)}>
+                                {channel.title}
+                            </div>
+                        ))}
+                    </div>
 				</div>
 			)}
+
+            {searchedUsers.length > 0 && (
+				<div className='users_div'>
+					<h2>People: </h2>
+					<div className="users_divs">
+                        {searchedUsers.map((user, ind) => (
+                            <div className='dm_pic_name' key={ind}>
+                                <img
+                                    className='dm_menu_img dm_chat_img'
+                                    onClick={(e) => onClickUser(e, user)}
+                                    src={user.profile_pic ? user.profile_pic : defaultProfileImage}
+                                    alt='navbar profile'
+                                />
+                                <div className='dm_chat_tm'>
+                                    <div
+                                        onClick={(e, user) => onClickUser(e, user)}
+                                        className='dm_user_name'>
+                                            {user.full_name.toLowerCase().includes(search.toLowerCase()) ? user.full_name : user.nickname}
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+				</div>
+			)}
+
 			{searchedCMS.length > 0 && (
 				<div className='dms_div'>
 					<h2>Channel Messages: </h2>
-					<div className="dms_messages">{searchedCMS}</div>
+					<div className="dms_messages">
+                        {searchedCMS.map((cm, ind) => (
+                            <div className='message_div' key={ind}>
+                                <div className='pic_name'>
+                                    <img
+                                        className='menu_img chat_img'
+                                        onClick={(e) => onClickUser(e, cm.user)}
+                                        src={cm.user.profile_pic ? cm.user.profile_pic : defaultProfileImage}
+                                        alt='navbar profile'
+                                    />
+                                    <div className='chat_tm'>
+                                        <div className='timestamp'>{createdAt(cm.created_at)}</div>
+                                        <div
+                                            onClick={(e) => onClickUser(e, cm.user)}
+                                            className='user_name'>
+                                                {`${cm.user.nickname ? cm.user.nickname : cm.user.full_name}:`}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='message_content' onClick={(e) => onClickChannel(e, cm.channel)}>
+                                    {`${cm.message}`} { cm.edited ? <p className="message_edited">(edited)</p> : <></> }
+                                </div>
+                            </div>
+                        ))}
+                    </div>
 				</div>
 			)}
             {searchedDMS.length > 0 && (
 				<div className='dms_div'>
 					<h2>Direct Messages: </h2>
-					<div className="dms_messages">{searchedDMS}</div>
+					<div className="dms_messages">
+                        {searchedDMS.map((dm, ind) => (
+                            <div className='message_div' key={ind}>
+                                <div className='pic_name'>
+                                    <img
+                                        className='menu_img chat_img'
+                                        onClick={(e) => onClickUser(e, dm.user)}
+                                        src={dm.user.profile_pic ? dm.user.profile_pic : defaultProfileImage}
+                                        alt='navbar profile'
+                                    />
+                                    <div className='chat_tm'>
+                                        <div className='timestamp'>{createdAt(dm.created_at)}</div>
+                                        <div
+                                            onClick={(e) => onClickUser(e, dm.user)}
+                                            className='user_name'>
+                                                {`${dm.user.nickname ? dm.user.nickname : dm.user.full_name}:`}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='message_content' onClick={(e) => onClickDM(e, dm.user)}>
+                                    {`${dm.message}`} { dm.edited ? <p className="message_edited">(edited)</p> : <></> }
+                                </div>
+                            </div>
+                        ))}
+                    </div>
 				</div>
 			)}
 		</div>
