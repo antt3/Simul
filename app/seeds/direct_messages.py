b453e3361b8c21b8c2273e7bebc387173902bc3e
Anthony T <anthonyt3@sbcglobal.net>
2022-12-11 20:04:59 -0800

    Unnecessary DMS Deleted & Date/Time Changes

--- app/seeds/direct_messages.py
+++ app/seeds/direct_messages.py
@@ -2,44 +2,33 @@ from app.models import db, Direct_message
 
 def seed_direct_messages():
     to_ref_1_1 = Direct_message(
-        message='First Message', edited=False, created_at='2022-08-10 10:1:23.473031', user_id=1, ref_id=1)
+        message='First Message', edited=False, created_at='2022-08-10 11:1:23.473031', user_id=1, ref_id=1)
     to_ref_1_1_2 = Direct_message(
-        message='Second Message', edited=False, created_at='2022-08-10 10:2:24.473031', user_id=1, ref_id=1)
+        message='Second Message', edited=False, created_at='2022-10-08 08:2:24.473031', user_id=1, ref_id=1)
     to_ref_1_1_3 = Direct_message(
-        message='Third Message', edited=False, created_at='2022-08-10 10:3:25.473031', user_id=1, ref_id=1)
+        message='Third Message', edited=False, created_at='2022-11-16 17:3:25.473031', user_id=1, ref_id=1)
     to_ref_1_1_4 = Direct_message(
-        message='Fourth Message', edited=False, created_at='2022-08-10 10:4:23.473031', user_id=1, ref_id=1)
+        message='Fourth Message', edited=False, created_at='2022-12-01 13:4:23.473031', user_id=1, ref_id=1)
     to_ref_1_2 = Direct_message(
-        message='First Message', edited=False, created_at='2018-08-10 10:2:23.473031', user_id=2, ref_id=1)
+        message='First Message', edited=False, created_at='2022-07-20 03:1:30.473031', user_id=2, ref_id=1)
     to_ref_1_3 = Direct_message(
-        message='First Message', edited=False, created_at='2018-08-10 10:3:23.473031', user_id=3, ref_id=1)
+        message='First Message', edited=False, created_at='2022-06-11 04:1:15.473031', user_id=3, ref_id=1)
     to_ref_1_4 = Direct_message(
-        message='First Message', edited=False, created_at='2018-08-10 10:4:23.473031', user_id=4, ref_id=1)
+        message='First Message', edited=False, created_at='2022-08-10 07:1:49.473031', user_id=4, ref_id=1)
     to_ref_2_1 = Direct_message(
-        message='Second Message', edited=False, created_at='2018-08-10 10:1:23.473031', user_id=2, ref_id=2)
-    to_ref_2_2 = Direct_message(
-        message='First Message', edited=False, created_at='2018-08-10 10:2:24.473031', user_id=3, ref_id=2)
-    to_ref_2_3 = Direct_message(
-        message='First Message', edited=False, created_at='2018-08-10 10:3:25.473031', user_id=4, ref_id=2)
-    to_ref_2_4 = Direct_message(
-        message='First Message', edited=False, created_at='2018-08-10 10:4:23.473031', user_id=1, ref_id=2)
+        message='Second Message', edited=False, created_at='2022-09-15 18:2:23.473031', user_id=2, ref_id=2)
     to_ref_3_1 = Direct_message(
-        message='Second Message', edited=False, created_at='2018-08-10 10:1:23.473031', user_id=3, ref_id=3)
-    to_ref_3_2 = Direct_message(
-        message='Second Message', edited=False, created_at='2018-08-10 10:2:23.473031', user_id=4, ref_id=3)
-    to_ref_3_3 = Direct_message(
-        message='First Message', edited=False, created_at='2018-08-10 10:3:23.473031', user_id=1, ref_id=3)
-    to_ref_3_4 = Direct_message(
-        message='First Message', edited=False, created_at='2018-08-10 10:4:23.473031', user_id=2, ref_id=3)
+        message='Second Message', edited=False, created_at='2022-07-23 20:2:54.473031', user_id=3, ref_id=3)
     to_ref_4_1 = Direct_message(
-        message='Second Message', edited=False, created_at='2018-08-10 10:1:23.473031', user_id=4, ref_id=4)
-    to_ref_4_2 = Direct_message(
-        message='Second Message', edited=False, created_at='2018-08-10 10:2:23.473031', user_id=1, ref_id=4)
-    to_ref_4_3 = Direct_message(
-        message='Second Message', edited=False, created_at='2018-08-10 10:3:23.473031', user_id=2, ref_id=4)
-    to_ref_4_4 = Direct_message(
-        message='First Message', edited=False, created_at='2018-08-10 10:4:23.473031', user_id=3, ref_id=4)
-    
+        message='Second Message', edited=False, created_at='2022-08-17 06:2:26.473031', user_id=4, ref_id=4)
+    to_ref_1_2_2 = Direct_message(
+        message='Third Message', edited=False, created_at='2022-10-21 04:3:12.473031', user_id=1, ref_id=2)
+    to_ref_2_1_2 = Direct_message(
+        message='Fourth Message', edited=False, created_at='2022-12-28 01:4:04.473031', user_id=2, ref_id=1)
+    to_ref_1_3_2 = Direct_message(
+        message='Third Message', edited=False, created_at='2022-09-12 12:3:07.473031', user_id=3, ref_id=4)
+    to_ref_3_1_2 = Direct_message(
+        message='Fourth Message', edited=False, created_at='2022-11-10 10:4:09.473031', user_id=3, ref_id=4)
 
     db.session.add(to_ref_1_1)
     db.session.add(to_ref_1_1_2)
@@ -49,17 +38,12 @@ def seed_direct_messages():
     db.session.add(to_ref_1_3)
     db.session.add(to_ref_1_4)
     db.session.add(to_ref_2_1)
-    db.session.add(to_ref_2_2)
-    db.session.add(to_ref_2_3)
-    db.session.add(to_ref_2_4)
     db.session.add(to_ref_3_1)
-    db.session.add(to_ref_3_2)
-    db.session.add(to_ref_3_3)
-    db.session.add(to_ref_3_4)
     db.session.add(to_ref_4_1)
-    db.session.add(to_ref_4_2)
-    db.session.add(to_ref_4_3)
-    db.session.add(to_ref_4_4)
+    db.session.add(to_ref_1_2_2)
+    db.session.add(to_ref_2_1_2)
+    db.session.add(to_ref_1_3_2)
+    db.session.add(to_ref_3_1_2)
 
     db.session.commit()
 
